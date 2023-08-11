b06dff45aaddbf07fcfb47efafbee2424c3c1d8c
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-08-10 20:43:54 -0500

    import image minimizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
