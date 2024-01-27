9dc728e14637e5877708b244f44fc25c4ddb3436
Jennifer Tieu <jennifer.tieuu@gmail.com>
2024-01-26 21:46:20 -0600

    replace image, add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        body: "#181402",
+        background: "#FDF9E7",
+        primary: "#FFED94",
+        secondary: "#F9EDB4",
+        accent: "#435CDB",
       },
     },
   },
