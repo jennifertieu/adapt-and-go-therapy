a36255975cb5e727d6ba3ed3122855d7a36075a6
Jennifer Tieu <jennifer.tieuu@gmail.com>
2024-07-23 21:15:54 -0500

    update images, update layout

--- app/page.js
+++ app/page.js
@@ -6,50 +6,46 @@ const playfairDisplay = Playfair_Display({
   weight: ["700"],
   style: "normal",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export default function Home() {
   return (
-    <main className="bg-background font-light">
-      <header className="bg-body flex justify-between p-4 w-full text-neutral-100">
+    <main className="font-light">
+      <header className="bg-background flex justify-between p-4 w-full text-neutral-100">
         <Image
           priority
           src={Logo}
           alt="Adapt and Go Therapy Logo"
           className="sm:max-w-64 max-w-40"
         />
-        <div className="flex gap-2">
-          <Image
-            src="/icons/phone-icon.png"
-            alt="Phone Icon"
-            width={24}
-            height={24}
-          />
-          <div className="justify-center items-center flex text-sm">
-            346-593-0721
-          </div>
-        </div>
       </header>
-      <section className="max-w-screen-xl">
-        <section className="relative flex justify-center sm:items-center sm:flex-row flex-col-reverse p-6 gap-6">
+      <section className="bg-background ">
+        <section className="max-w-screen-xl m-auto relative flex justify-center sm:items-center sm:flex-row flex-col-reverse p-6 gap-6">
           <article>
             <h1
-              className={`${playfairDisplay.className} text-3xl sm:text-6xl sm:leading-[1.5] leading-normal mb-2`}
+              className={`${playfairDisplay.className} font-bold text-3xl sm:text-6xl sm:leading-[1.5] leading-normal mb-4`}
             >
               Empowering Lives, <br /> Healing Homes
             </h1>
-            <p className="mb-2 text-xl sm:text-4xl sm:absolute">
+            <p className="mb-8 text-lg sm:text-2xl">
               Where licensed therapists come to you
             </p>
+            <button className="bg-accent text-lg  text-white px-10 rounded-3xl py-3">
+              Contact Us
+            </button>
           </article>
           <Image
             src="/img/old-woman-nursing-home-walking-with-crutches-with-help-from-female-nurse.jpg"
             alt=""
             width={600}
             height={950}
-            className="max-w-lg rounded"
+            className="max-w-lg rounded-3xl"
           />
         </section>
+      </section>
+      <section className="bg-white">
+        <h2>Amazing</h2>
       </section>
     </main>
   );
